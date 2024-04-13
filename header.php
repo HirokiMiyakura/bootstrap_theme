34b1c69fa12d7886a832fdd40bb189e31593f5cb
HirokiMiyakura <miyakurahiroki@gmail.com>
2024-04-13 13:02:37 +0900

    ハンバーガーメニュー追加

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 <link href="<?php echo get_template_directory_uri(); ?>/assets/css/ress.css" rel="stylesheet" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 <link href="<?php echo get_template_directory_uri(); ?>/assets/scss/main.css" rel="stylesheet" />
+<link href="<?php echo get_template_directory_uri(); ?>/assets/scss/hamburger.css" rel="stylesheet" />
 <?php wp_head(); ?>
 </head>
 <body>
